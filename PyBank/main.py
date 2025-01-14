04dc6d18ffa16b8cda09b7bbaac75fbd863f9cae
kieranto1204 <kieranto1204@csu.fullerton.edu>
2025-01-13 21:42:24 -0800

    Fixed file directory for PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,8 +6,8 @@ import csv
 import os
 
 # Files to load and output (update with correct file paths)
-file_to_load = os.path.join("PyBank", "Resources", "budget_data.csv")  # Input file path
-file_to_output = os.path.join("PyBank","analysis", "budget_analysis.txt")  # Output file path
+file_to_load = os.path.join("python-challenge","PyBank", "Resources", "budget_data.csv")  # Input file path
+file_to_output = os.path.join("python-challenge","PyBank","analysis", "budget_analysis.txt")  # Output file path
 
 # Define variables to track the financial data
 total_months = 0
